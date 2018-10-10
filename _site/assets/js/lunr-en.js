507e080043bde6af2e4ae18fc5e02ab48a94758f
Soumita Sengupta <soumita.s@insync.co.in>
2018-10-10 18:13:16 +0530

    Updated

--- _site/assets/js/lunr-en.js
+++ _site/assets/js/lunr-en.js
@@ -77,7 +77,7 @@ var idx = lunr(function () {
     
   
     idx.add({
-      title: "Getting started to APPSeCONNECT",
+      title: "Getting Started to APPSeCONNECT",
       excerpt: "CUSTOMER PROFILE INFORMATION This section enables you to quickly access your profile after logging into the application. This is where...",
       categories: ["APPSeCONNECT"],
       tags: ["developers"],
@@ -681,7 +681,7 @@ var store = [
     
       
       {
-        "title": "Getting started to APPSeCONNECT",
+        "title": "Getting Started to APPSeCONNECT",
         "url": "/appseconnect/getting-started/",
         "excerpt": "CUSTOMER PROFILE INFORMATION This section enables you to quickly access your profile after logging into the application. This is where...",
         "teaser":
