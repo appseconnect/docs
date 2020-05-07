5cb77d72083124a3c0452e5902ad65d1a63aaaad
Soumita Sengupta <Soumita.s@insync.co.in>
2020-05-07 13:24:27 +0530

    update PF management

--- _site/assets/js/lunr-en.js
+++ _site/assets/js/lunr-en.js
@@ -2114,20 +2114,11 @@ var idx = lunr(function () {
     
   
     idx.add({
-      title: "Protocol Implementation for Applications",
-      excerpt: "Protocol Implementation for Applications in Process Flow. Applications are built with protocols for making them communicate with network resources and...",
-      categories: ["Processflow"],
-      tags: ["developers"],
-      id: 233
-    });
-    
-  
-    idx.add({
       title: "Delete Processflow",
       excerpt: "Deleting option enables you to delete a process flow once you have designed it, deployed it or executed it. Deletion...",
       categories: ["Processflow"],
       tags: ["developers"],
-      id: 234
+      id: 233
     });
     
   
@@ -2136,7 +2127,7 @@ var idx = lunr(function () {
       excerpt: "Once the Collection and the Reference tables are created, users can implement those lookups in the attribute mapping for the...",
       categories: ["Processflow"],
       tags: ["developers"],
-      id: 235
+      id: 234
     });
     
   
@@ -2145,7 +2136,7 @@ var idx = lunr(function () {
       excerpt: "Repositories in APPSeCONNECT provides the user to create and manage Lookup types in form of tables for the organisation. Lookups...",
       categories: ["Processflow"],
       tags: ["developers"],
-      id: 236
+      id: 235
     });
     
   
@@ -2154,7 +2145,7 @@ var idx = lunr(function () {
       excerpt: "Mapping in APPSeCONNECT is done so that a Source Application can communicate with the Destination Application. APPSeCONNECT, introduces a new...",
       categories: ["Processflow"],
       tags: ["developers"],
-      id: 237
+      id: 236
     });
     
   
@@ -2163,7 +2154,7 @@ var idx = lunr(function () {
       excerpt: "The data coming from the source schema requires to be transformed individually such that it matches the format of the...",
       categories: ["Processflow"],
       tags: ["developers"],
-      id: 238
+      id: 237
     });
     
   
@@ -2172,7 +2163,7 @@ var idx = lunr(function () {
       excerpt: "Here you will understand the process of defining Variables (any variables that stores the data that can be used later...",
       categories: ["Processflow"],
       tags: ["developers"],
-      id: 239
+      id: 238
     });
     
   
@@ -2181,7 +2172,7 @@ var idx = lunr(function () {
       excerpt: "XPATH is an essential concept that is required when implementing mapping and transformations. XPATH access nodes in a hierarchical structure...",
       categories: ["Processflow"],
       tags: ["developers"],
-      id: 240
+      id: 239
     });
     
   
@@ -2190,7 +2181,7 @@ var idx = lunr(function () {
       excerpt: "Namespace is a technique that defines a set of unique names within an XML file, consisting of several XML elements...",
       categories: ["Processflow"],
       tags: ["developers"],
-      id: 241
+      id: 240
     });
     
   
@@ -2199,7 +2190,7 @@ var idx = lunr(function () {
       excerpt: "Renderer enables the user to alter the default XML structure which and when required through rendering. In case of Complex...",
       categories: ["Processflow"],
       tags: ["developers"],
-      id: 242
+      id: 241
     });
     
   
@@ -2208,7 +2199,7 @@ var idx = lunr(function () {
       excerpt: "Application configuration is an integral activity prior to the process of integration. If your chosen application is Salesforce, credentials need...",
       categories: ["Connectors"],
       tags: ["developers"],
-      id: 243
+      id: 242
     });
     
   
@@ -2217,7 +2208,7 @@ var idx = lunr(function () {
       excerpt: "Given below is the list of elements you will be able to use in APPSeCONNECT Process Flow. You can use...",
       categories: ["Processflow"],
       tags: ["developers"],
-      id: 244
+      id: 243
     });
     
   
@@ -2226,7 +2217,7 @@ var idx = lunr(function () {
       excerpt: "When working with Process Flows, it is essential to map the attributes and the entities of both source and destination...",
       categories: ["Processflow"],
       tags: ["developers"],
-      id: 245
+      id: 244
     });
     
   
@@ -2235,7 +2226,7 @@ var idx = lunr(function () {
       excerpt: "Why do you need APPS? For transforming data from one application to another you need to drag Application nodes to...",
       categories: ["Processflow"],
       tags: ["developers"],
-      id: 246
+      id: 245
     });
     
   
@@ -2244,7 +2235,7 @@ var idx = lunr(function () {
       excerpt: "What is Get Node? GET node is used for getting data from an application by using their API Endpoints provided...",
       categories: ["Processflow"],
       tags: ["developers"],
-      id: 247
+      id: 246
     });
     
   
@@ -2253,34 +2244,7 @@ var idx = lunr(function () {
       excerpt: "What is POST Node? POST node is used for pushing transformed data to the destination application by using API Endpoints...",
       categories: ["Processflow"],
       tags: ["developers"],
-      id: 248
-    });
-    
-  
-    idx.add({
-      title: "Splitter Node",
-      excerpt: "What is Splitter Node? This node is used to split a data packet into smaller chunks for processing in small...",
-      categories: ["Processflow"],
-      tags: ["developers"],
-      id: 249
-    });
-    
-  
-    idx.add({
-      title: "Cloud Appresource Functions",
-      excerpt: "Cloud Appresource Functions for Process Flow Appresource Functions are the functions in APPSeCONNECT that are written in the adapter which...",
-      categories: ["Processflow"],
-      tags: ["developers"],
-      id: 250
-    });
-    
-  
-    idx.add({
-      title: "Merger Node",
-      excerpt: "What is Merger Node? This node is used to merge smaller data chunks into a single data packet. Like a...",
-      categories: ["Processflow"],
-      tags: ["developers"],
-      id: 251
+      id: 247
     });
     
   
@@ -4866,17 +4830,6 @@ var store = [
     
       
       {
-        "title": "Protocol Implementation for Applications",
-        "url": "/processflow/protocol-implementation/",
-        "excerpt": "Protocol Implementation for Applications in Process Flow. Applications are built with protocols for making them communicate with network resources and...",
-        "teaser":
-          
-            "/assets/images/APPSeCONNECT-documentation.png"
-          
-      },
-    
-      
-      {
         "title": "Delete Processflow",
         "url": "/processflow/delete-processflow/",
         "excerpt": "Deleting option enables you to delete a process flow once you have designed it, deployed it or executed it. Deletion...",
@@ -5034,39 +4987,6 @@ var store = [
         "title": "Working with POST",
         "url": "/processflow/working-with-POST/",
         "excerpt": "What is POST Node? POST node is used for pushing transformed data to the destination application by using API Endpoints...",
-        "teaser":
-          
-            "/assets/images/APPSeCONNECT-documentation.png"
-          
-      },
-    
-      
-      {
-        "title": "Splitter Node",
-        "url": "/processflow/working-with-processflow-splitter/",
-        "excerpt": "What is Splitter Node? This node is used to split a data packet into smaller chunks for processing in small...",
-        "teaser":
-          
-            "/assets/images/APPSeCONNECT-documentation.png"
-          
-      },
-    
-      
-      {
-        "title": "Cloud Appresource Functions",
-        "url": "/processflow/cloud-appresource-functions/",
-        "excerpt": "Cloud Appresource Functions for Process Flow Appresource Functions are the functions in APPSeCONNECT that are written in the adapter which...",
-        "teaser":
-          
-            "/assets/images/APPSeCONNECT-documentation.png"
-          
-      },
-    
-      
-      {
-        "title": "Merger Node",
-        "url": "/processflow/working-with-processflow-merger/",
-        "excerpt": "What is Merger Node? This node is used to merge smaller data chunks into a single data packet. Like a...",
         "teaser":
           
             "/assets/images/APPSeCONNECT-documentation.png"
